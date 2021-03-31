6f4827b7728d6e7f590b62b7e89071ed2b2a5ed6
biruk lealem <biruktelavive7@gmail.com>
2021-03-31 00:15:26 -0700

    java version

---
+++ 30-days-of-code/Day_1_Data_TYpes.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Day_1_Data_Types {
+    public static void main (String args []){
+        Scanner input = new Scanner(System.in);
+        
+        int i = 4;
+double d = 4.0;
+String s = "HackerRank ";
+
+//Declare second integer, double, and String variables.
+
+int integer = input.nextInt();
+double doubleNum =input.nextDouble();
+String stringVar = input.nextLine();
+
+//Read and save an integer, double and String to your variables.
+
+//Print the sum of both integer variables on a new line.
+System.out.println( i + integer);
+
+//Print the sum of the double variables on a new line.
+System.out.printf( "%.1f\n" , (d + doubleNum));
+
+//Concatenate and print the String variables on a new line.
+System.out.println ( s + stringVar);
+
+//The 's' variable above should be printed first.
+
+    }
+} 
+
+
